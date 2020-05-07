31bc6e4b70099ce1662dcf665bc11f7732369d08
Yuma Kishi <obake2ai@gmail.com>
2020-05-07 14:59:46 +0900

    bug fix

--- prepare_data.py
+++ prepare_data.py
@@ -65,18 +65,18 @@ if __name__ == '__main__':
     parser.add_argument('--size', type=str, default='128,256,512,1024')
     parser.add_argument('--n_worker', type=int, default=8)
     parser.add_argument('--resample', type=str, default='lanczos')
-    parser.add_argument('path', type=str)
+    parser.add_argument('--input', type=str)
 
     args = parser.parse_args()
-    
+
     resample_map = {'lanczos': Image.LANCZOS, 'bilinear': Image.BILINEAR}
     resample = resample_map[args.resample]
-    
+
     sizes = [int(s.strip()) for s in args.size.split(',')]
 
     print(f'Make dataset of image sizes:', ', '.join(str(s) for s in sizes))
 
-    imgset = datasets.ImageFolder(args.path)
+    imgset = datasets.ImageFolder(args.input)
 
     with lmdb.open(args.out, map_size=1024 ** 4, readahead=False) as env:
         prepare(env, imgset, args.n_worker, sizes=sizes, resample=resample)
